2891760e5009c2d9481188302714d2be295b4601
Török Ákos <torok501akos@sch.bme.hu>
2025-11-11 13:05:20 +0100

    File handlers commit

---
+++ src/Model/IOs/CSVParser.java
@@ -0,0 +1,11 @@
+package Model.IOs;
+
+import model.*;
+import model.Pieces.*;
+
+import java.io.*;
+import java.util.*;
+import java.nio.file.*;
+
+public class CSVParser{
+}
---
+++ src/Model/IOs/JSONPersistance.java
@@ -0,0 +1,48 @@
+package Model.IOs;
+
+import model.*;
+
+import org.json.*;
+import java.io.*;
+import java.nio.file.*;
+
+public class JSONPersistance{
+    private final Gson gson;
+
+    public JsonPersistence() {
+        // Use pretty printing for readable JSON files (good for debugging).
+        this.gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+    }
+
+    /**
+     * Serialises the entire GameState object to a JSON file.
+     *
+     * @param state The current GameState to save.
+     * @param filePath The path to the file where the game will be saved.
+     * @throws IOException if an error occurs during writing.
+     */
+    public void saveGame(GameState state, String filePath) throws IOException {
+        // Use try-with-resources to ensure the writer is closed automatically.
+        try (Writer writer = new FileWriter(filePath)) {
+            gson.toJson(state, writer);
+        }
+    }
+
+    /**
+     * Deserialises a GameState object from a JSON file.
+     *
+     * @param filePath The path to the file to load.
+     * @return The reconstructed GameState object.
+     * @throws IOException if an error occurs during reading.
+     */
+    public GameState loadGame(String filePath) throws IOException {
+        // Use try-with-resources to ensure the reader is closed.
+        try (Reader reader = Files.newBufferedReader(Paths.get(filePath))) {
+            // Gson handles the reconstruction of the entire object graph.
+            GameState state = gson.fromJson(reader, GameState.class);
+            return state;
+        }
+    }
+}
